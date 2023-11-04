bc825a99ee62cda9079d93c37fa1163952439980
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-11-04 13:43:58 +0200

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Header.h"
+
+int main()
+{
+    const int size = 5;
+    int arr[size] = { 45, 32, 12, 9, 4 };
+    ShowArray(arr,size);
+    cout << "\n";
+    cout << "Minimum: " << GetMin(arr, size) << "\n";
+    cout << "Maximum: " << GetMax(arr, size) << "\n";
+
+
+    return 0;
+}
+
